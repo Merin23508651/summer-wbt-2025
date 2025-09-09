979bc2e61d4b653fc464494ddb01addf04862f4f
Merin Taj <merintaj3@gmail.com>
2025-09-09 07:18:49 +0600

    Update dashboard.php

--- Xccompany/php/dashboard.php
+++ Xccompany/php/dashboard.php
@@ -6,39 +6,37 @@
   </head>
   <body>
     <header>
-        <div>
-          class="navbar">
-            <div> class="company">
-                <img src="../picture/logo.png" alt="Company Logo" class="logo">
-                <span>Company</span>
-            </div
-            
-            <div class="user-info">
-                Logged in as | <a href="login.php">Logout</a>
-            </div>
+      <div class="navbar">
+        <div class="company">
+          <img src="../picture/logo.png" alt="Company Logo" class="logo">
+          <span>Company</span>
         </div>
+        <div class="user-info">
+          Logged in as | <a href="login.php">Logout</a>
+        </div>
+      </div>
     </header>
            
-     <div class="container">
-        <aside class="sidebar">
-            <h3>Account</h3>
-            <ul>
-                <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="view_profile.php">View Profile</a></li>
-                <li><a href="edit_profile.php">Edit Profile</a></li>
-                <li><a href="profile_picture.php">Change Profile Picture</a></li>
-                <li><a href="change_password.php">Change Password</a></li>
-                <li><a href="login.php">Logout</a></li>
-            </ul>
-        </aside>
-        
-        <main class="content">
-            <h1>Welcome</h1>
-        </main>
-      </div>
+    <div class="container">
+      <aside class="sidebar">
+        <h3>Account</h3>
+        <ul>
+          <li><a href="dashboard.php">Dashboard</a></li>
+          <li><a href="view_profile.php">View Profile</a></li>
+          <li><a href="edit_profile.php">Edit Profile</a></li>
+          <li><a href="profile_picture.php">Change Profile Picture</a></li>
+          <li><a href="change_password.php">Change Password</a></li>
+          <li><a href="login.php">Logout</a></li>
+        </ul>
+      </aside>
+      
+      <main class="content">
+        <h1>Welcome</h1>
+      </main>
+    </div>
     
     <footer>
-        <p>Copyright © 2017</p>
+      <p>Copyright © 2017</p>
     </footer>
- </body>
+  </body>
 </html>
