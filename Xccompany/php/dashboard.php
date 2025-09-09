1c477d87fb3fe68955463990119c1a5ac99d03a2
merin taj <you@example.com>
2025-09-09 07:10:19 +0600

    re-push

--- Xccompany/php/dashboard.php
+++ Xccompany/php/dashboard.php
@@ -1,30 +1,22 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
     <title>Dashboard</title>
-    <link rel="stylesheet" type="text/css" href="../styles/dashboard.css">
+    <link rel="stylesheet" href="your-style.css">
 </head>
 <body>
     <header>
-        <div class="navbar">
-            <div class="company">
-                <img src="../picture/logo.png" alt="Company Logo" class="logo">
-                <span>Company</span>
-            </div>
-            <div class="user-info">
-                Logged in as | <a href="login.php">Logout</a>
-            </div>
+        <div>
+            <h2>Dashboard Header</h2>
         </div>
     </header>
     <div class="container">
         <aside class="sidebar">
             <h3>Account</h3>
             <ul>
-                <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="view_profile.php">View Profile</a></li>
-                <li><a href="edit_profile.php">Edit Profile</a></li>
-                <li><a href="profile_picture.php">Change Profile Picture</a></li>
-                <li><a href="change_password.php">Change Password</a></li>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="settings.php">Settings</a></li>
                 <li><a href="login.php">Logout</a></li>
             </ul>
         </aside>
