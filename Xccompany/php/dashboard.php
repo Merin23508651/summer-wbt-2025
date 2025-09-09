7bda06355b004b5612a0db49f8ddf0a702e476a6
Merin Taj <merintaj3@gmail.com>
2025-09-09 07:16:47 +0600

    Update dashboard.php

--- Xccompany/php/dashboard.php
+++ Xccompany/php/dashboard.php
@@ -7,10 +7,10 @@
   <body>
     <header>
         <div class="navbar">
-            <div class="company">
+            <div> class="company">
                 <img src="../picture/logo.png" alt="Company Logo" class="logo">
-                <span>Company</span>
-            </div>
+                <span>Company</span></div
+            
             <div class="user-info">
                 Logged in as | <a href="login.php">Logout</a>
             </div>
